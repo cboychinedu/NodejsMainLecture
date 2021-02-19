b8550d3e4346f753b859f2f34e7f3d46151ad8b6
Mbonu Chinedu <cboy.chinedu@gmail.com>
2021-02-19 20:16:00 +0100

    mongodb-database-connection

---
+++ mongodb-connection/connectDb.js
@@ -0,0 +1,41 @@
+// Importing the necessary modules 
+const mongodb = require('mongoose');
+
+// Connecting 
+mongodb.connect('mongodb://localhost/local_user')
+    .then(() => {console.log('Connected to mongodb...')}) 
+    .catch((err) => { console.log('Could not connect to mongodb...')}); 
+
+// Creating a schema 
+const courseSchema = new mongodb.Schema({
+    name: String, 
+    author: String, 
+    tags: [String ],
+    date: { type: Date, default: Date.now }, 
+    isPublished: Boolean 
+
+}); 
+
+// Converting the created schema into a model 
+const COURSE = mongodb.model('Course', courseSchema); 
+
+// Creating an async function to connect to the mongodb database and save the 
+// necessary information. 
+async function createCourse()
+{
+    // Creating an object from the created model/class 
+    const course = new COURSE({
+        name: "Nodejs Course", 
+        author: "Mosh Lectures", 
+        tags: ["node", "backend"], 
+        isPublished: true 
+    }); 
+
+    // Saving the couse into the database  
+    const result = await course.save(); 
+    console.log(result); 
+
+}
+
+// Running the function 
+createCourse(); 
---
+++ mongodb-connection/retriveDocuments.js
@@ -0,0 +1,81 @@
+// Importing the necessary modules 
+const mongodb = require('mongoose'); 
+
+// Connecting to the mongodb database  
+mongodb.connect('mongodb://localhost/local_user')
+    .then(() => { console.log('Connected to the mongodb database..')}) 
+    .catch(() => { console.log('Could not connect to mongodb database...')}); 
+
+// Creating a schema 
+const courseSchema = new mongodb.Schema({
+    name: String, 
+    author: String, 
+    tags: [String, ], 
+    date: { type: Date, default: Date.now }, 
+    isPublished: Boolean 
+
+}); 
+
+// Converting the created schema into a model 
+const COURSE = mongodb.model('Course', courseSchema); 
+
+// Creating an async function to get the courses from the database 
+async function getCourses() 
+{
+    // Creating an async await class to get all the courses 
+    const courses = await COURSE
+        .find(); 
+
+    // Displaying the result 
+    console.log(courses); 
+}; 
+
+// Creating an async function to get the courses by a specific property 
+async function getCourseByProp()
+{
+    // Creating an async await function to get the courses by author and isPublished 
+    const courses = await COURSE
+        .find({ author: 'Mosh Lectures', isPublished: true })
+        .limit(10) 
+        // .sort({ name: 1, author: 1 })
+        .select({ name: 1, tags: 1, author: 1 }); 
+    
+    // Displaying the result 
+    console.log(courses); 
+}
+
+/* IMPORTANT OPERANDS */ 
+/*  eq  ( equal ) 
+*   ne  ( not equal to) 
+*   gt  ( greater than )
+*   gte ( greater than or equal to ) 
+*   lt  ( less than or equal to ) 
+*   lte  ( less than or equal to ) 
+*   in   ( inside the ) 
+*   nin  ( not inside the ) 
+*/
+
+// Creating an async function to get the course by using the comparison operator 
+async function getCourseByComparison()
+{
+    // Creating an async await function to get the courses by comparison 
+    const courses = await COURSE
+        //---  looking for courses with the price greater than 10 
+        /* .find({ price: { $gt: 10 } })  */ 
+
+        //---  looking for courses with the price greater than or equal to 10, and less than or equal to 20  
+        /* .find({ price: { $gte: 10, $lte: 20 }})  */ 
+
+        //--- looking for courses with the given price range values 
+        .find({ price: { $in: [20, 30, 40, 50] } })
+        .limit(10)
+        .select({ name: 1, tags: 1, author: 1 }); 
+
+    // Displaying the results 
+    console.log(courses); 
+
+}
+
+// Executing the function 
+// getCourses();
+getCourseByProp();  
