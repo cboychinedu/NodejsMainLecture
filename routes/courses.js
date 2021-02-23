b41b5e623c532d7da964cb52bb543e0f61d61676
Mbonu Chinedu <cboy.chinedu@gmail.com>
2021-02-23 04:51:39 +0100

    connected to mongo

--- routes/courses.js
+++ routes/courses.js
@@ -14,15 +14,17 @@ const Joi = require('joi');
 
 // Creating a schema 
 const courseSchema = new mongodb.Schema({
-    "_id": Number, 
-    "FirstName": String, 
-    "LastName": String, 
+    name: { type: String, required: true }, 
+    author: String, 
+    tags: [String, ], 
     date: { type: Date, default: Date.now }, 
+    price: Number, 
+    isPublished: Boolean, 
     versionKey: false 
 }); 
 
-// Converting the created schema into a model 
-const COURSE = mongodb.model('courses', courseSchema); 
+// Using the collection "courses" with the created courseSchema 
+const COURSE = mongodb.model('Courses', courseSchema); 
 
 
 // Creating a function for validating the course 
@@ -42,7 +44,7 @@ router.get('/', async (req, res) =>
     // If successful, send back all the courses 
     const courses = await COURSE
         .find()
-        .select({"FirstName": 1, "LastName": 1, "_id": 1 }); 
+        .select({name: 1, author: 1, tags: 1, price: 1, isPublished: 1 })
     
     // Sending back the courses 
     res.send(courses); 
@@ -53,7 +55,8 @@ router.get('/', async (req, res) =>
 router.post('/', async (req, res) => 
 {
     // 
-    const result = validateCourse(req.body);
+    const result = validateCourse({name: req.body.name});
+    // console.log(req.body.name); 
     // console.log(result.error); 
 
     // Checking for errors 
@@ -63,7 +66,10 @@ router.post('/', async (req, res) =>
     // Creating a dict to hold newly created values using POST request 
     let courseObj = new COURSE ({ 
         // id: courses.length + 1,   // Getting the id manually 
-        name: req.body.name       // Getting the name from the post request 
+        name: req.body.name,       // Getting the name from the post request 
+        author: req.body.author, 
+        tags: req.body.tags, 
+        isPublished: req.body.isPublished
     }); 
 
     // Pushing the created value to the courses dict 
@@ -74,12 +80,13 @@ router.post('/', async (req, res) =>
 }); 
 
 // Updating resources (PUT) (UPDATE)
-router.put('/:id', (req, res) => 
+router.put('/:id', async (req, res) => 
 {
     // Look up the course 
     // if the course not existing, return error 404 
-    let course = courses.find((c => c.id === parseInt(req.params.id))); 
-
+    // let course = courses.find((c => c.id === parseInt(req.params.id))); 
+    let course = await COURSE.findById({"_id": req.params.id})
+    
     if (course) { res.send(course); res.end(); }
     else return res.status(404).send('The course with the given id was not found.'); 
 
@@ -96,31 +103,34 @@ router.put('/:id', (req, res) =>
 }); 
 
 // http DELETE REQUEST 
-router.delete('/:id', (req, res) => 
+router.delete('/:id', async (req, res) => 
 {
     // look up the course 
-    let course = courses.find((c => c.id === parseInt(req.params.id))); 
+    // let course = courses.find((c => c.id === parseInt(req.params.id))); 
+    let course = await COURSE.findById({"_id": req.params.id}); 
 
     // 
     if (course) { res.send(course); res.end(); }
     else return res.status(404).send(`The course with the given id was not found.`); 
 
     // Delete the course 
-    const index = courses.indexOf(course)
-    courses.splice(index, 1); 
+    // const index = courses.indexOf(course)
+    // courses.splice(index, 1);
+    const result = await COURSE.findByIdAndRemove(req.params.id);  
 
     // Return the response 
-    res.send(course) 
+    res.send(result);  
     res.close(); 
 })
 
 // Getting parameters firstly (id) before sending back the resource. 
-router.get('/:id', (req, res) => 
+router.get('/:id', async (req, res) => 
 {
     // Checking for the courses with the specific given id-pair to see if present. 
-    let course = courses.find((c => c.id === parseInt(req.params.id))); 
+    // let course = courses.find((c => c.id === parseInt(req.params.id))); 
+    let course = await COURSE.findById({"_id": req.params.id}); 
 
-    // Checking if the course does not have a value 
+    // Checking if the course has a value  
     if (course) { res.send(course); res.end(); }
     else return res.status(404).send('The course with the given id was not found.');  
 }); 
